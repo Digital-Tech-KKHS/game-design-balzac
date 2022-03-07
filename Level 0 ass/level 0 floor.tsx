7c0f3edf55cac1b8eb461b06393908e3d4d9f021
Lenkwolf <puggychien@gmail.com>
2022-03-08 09:25:28 +1300

    changed stuff

---
+++ Level 0 ass/level 0 floor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="level 0 floor" tilewidth="1280" tileheight="960" tilecount="1" columns="1">
+ <image source="floor.png" width="1280" height="960"/>
+</tileset>
