d188a1b46569f9e0c165230c3c82b00ba7692e24
Lenkwolf <puggychien@gmail.com>
2022-03-11 14:07:49 +1300

    m

--- Level 0 assets/Wall.tsx
+++ Level 0 assets/Wall.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="Wall" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="../../Wall.png" width="32" height="32"/>
+<tileset version="1.8" tiledversion="1.8.2" name="Wall" tilewidth="320" tileheight="320" tilecount="36" columns="6">
+ <image source="Wall.png" width="1920" height="1920"/>
 </tileset>
