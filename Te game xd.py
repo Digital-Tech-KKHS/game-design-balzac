268ebe57557453d820d228c5da972a7bf595f80a
Lenkwolf <puggychien@gmail.com>
2022-03-16 12:13:41 +1300

    Merge branch 'master' of https://github.com/Digital-Tech-KKHS/game-design-balzac

--- Te game xd.py
+++ Te game xd.py
@@ -11,7 +11,7 @@ CHARACTER_SCALING = 0.4
 CURSOR_SCALING = 0.2
 PLAYER_MOVEMENT_SPEED = 7
 AMBIENT_COLOR = (1, 1, 1)
-
+TILE_SCALING = 0.4
 SPRINT_SPEED = 3
 
 
@@ -111,20 +111,23 @@ class MyGame(arcade.Window):
         self.light_layer = None
         self.player_light = None
         self.sprinting = False
+        self.scene = None
 
         arcade.set_background_color(arcade.color_from_hex_string("#7b692f"))
 
     def setup(self):
+        tile_map = arcade.load_tilemap("Level 0 assets\level_1.tmx", TILE_SCALING)
+        self.scene = arcade.Scene.from_tilemap(tile_map)
 
-        self.player_list = arcade.SpriteList()
+        self.scene.add_sprite_list('legs_list')
+        self.scene.add_sprite_list('player_list')
         self.cursor_list = arcade.SpriteList()
-        self.legs_list = arcade.SpriteList()
         self.floor = arcade.load_texture("floor.png")
         player = "dude.png"
         self.player_sprite = arcade.Sprite(player, CHARACTER_SCALING)
-        self.player_sprite.center_x = 320
-        self.player_sprite.center_y = 240
-        self.player_list.append(self.player_sprite)
+        self.player_sprite.center_x = 9601
+        self.player_sprite.center_y = 6272
+        self.scene['player_list'].append(self.player_sprite)
         self.player_sprite.angle = 180
         cursor = "cursor.png"
         self.cursor_sprite = arcade.Sprite(cursor, CURSOR_SCALING)
@@ -132,17 +135,14 @@ class MyGame(arcade.Window):
         self.legs_sprite = PlayerCharacter()
         self.legs_sprite.center_x = self.player_sprite.center_x
         self.legs_sprite.center_y = self.player_sprite.center_y
-        self.legs_list.append(self.legs_sprite)
+        self.scene['legs_list'].append(self.legs_sprite)
         self.set_mouse_visible(False)
 
         self.light_layer = LightLayer(SCREEN_WIDTH, SCREEN_HEIGHT)
 
-        x = 100
-        y = 200
-        radius = 500
-        mode = 'soft'
-        color = arcade.csscolor.GRAY
-        light = Light(x, y, radius, color, mode)
+        for sprite in self.scene['lights']:
+            print(sprite.center_y)
+            light = Light(sprite.center_x, sprite.center_y, sprite.properties['radius'], color=sprite.properties['color'][:3], mode='soft')
         self.light_layer.add(light)
 
         radius = 300
@@ -155,18 +155,13 @@ class MyGame(arcade.Window):
 
         self.clear()
 
-        self.legs_list.draw()
-        self.player_list.draw()
-
         with self.light_layer:
             self.clear()
             arcade.draw_lrwh_rectangle_textured(0,0,1280,960,self.floor)
-            self.legs_list.draw()
-            self.player_list.draw()
+            self.scene.draw()
 
-        self.light_layer.draw(ambient_color=AMBIENT_COLOR)
+            self.light_layer.draw(ambient_color=AMBIENT_COLOR)
         self.cursor_list.draw()
-
     def on_resize(self, width, height):
         self.light_layer.resize(width, height)
         
@@ -239,15 +234,14 @@ class MyGame(arcade.Window):
         self.legs_sprite.update(delta_time)
         self.cursor_sprite.center_x = self._mouse_x + self.get_viewport()[0]
         self.cursor_sprite.center_y = self._mouse_y + self.get_viewport()[2]
-        for player in self.player_list:
-            start_x = self.player_sprite.center_x
-            start_y = self.player_sprite.center_y
-            dest_x = self.cursor_sprite.center_x
-            dest_y = self.cursor_sprite.center_y
-            x_diff = dest_x - start_x
-            y_diff = dest_y - start_y
-            angle = math.atan2(y_diff, x_diff)
-            self.player_sprite.angle = math.degrees(angle) - 90
+        start_x = self.player_sprite.center_x
+        start_y = self.player_sprite.center_y
+        dest_x = self.cursor_sprite.center_x
+        dest_y = self.cursor_sprite.center_y
+        x_diff = dest_x - start_x
+        y_diff = dest_y - start_y
+        angle = math.atan2(y_diff, x_diff)
+        self.player_sprite.angle = math.degrees(angle) - 90
         
         self.player_light.position = self.player_sprite.position
 
