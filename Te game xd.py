230a28e3c353fb2d840fa58b2c1839e50f41c909
Lenkwolf <puggychien@gmail.com>
2022-03-17 17:18:10 +1300

    fixed speedylegs

--- Te game xd.py
+++ Te game xd.py
@@ -44,8 +44,8 @@ class PlayerCharacter(arcade.Sprite):
 
     def update_animation(self, delta_time: float = 1 / 60):
 
-        if self.change_x < 0 and self.character_face_direction == RIGHT_FACING:
-            self.character_face_direction = LEFT_FACING
+        if self.change_x < 0 and self.character_face_direction == LEFT_FACING:
+            self.character_face_direction = RIGHT_FACING
         elif self.change_x > 0 and self.character_face_direction == LEFT_FACING:
             self.character_face_direction = RIGHT_FACING
         if self.change_y < 0 and self.character_face_direction == LEFT_FACING:
@@ -59,14 +59,7 @@ class PlayerCharacter(arcade.Sprite):
         elif self.change_y == 0:
             self.texture = self.idle_texture_pair[self.character_face_direction]
 
-        if self.change_x != 0:
-            self.cur_texture += 1
-            if self.cur_texture > len(self.walk_textures) - 1:
-                self.cur_texture = 0
-            self.texture = self.walk_textures[self.cur_texture][
-                self.character_face_direction
-            ]
-        if self.change_y != 0:
+        if self.change_x or self.change_y != 0:
             self.cur_texture += 1
             if self.cur_texture > len(self.walk_textures) - 1:
                 self.cur_texture = 0
