a0c6d4c8bdc2f25615fda7a7a8a0ab3edcba2494
Lenkwolf <puggychien@gmail.com>
2022-03-25 14:57:22 +1300

    man and cameras

--- Te game xd.py
+++ Te game xd.py
@@ -184,8 +184,8 @@ class MyGame(arcade.Window):
         
         self.clear()
 
-        self.camera.use()
-        self.HUD_camera.use()
+        # self.camera.use()
+        # self.HUD_camera.use()
         self.sprint_bar.draw()
 
         with self.light_layer:
@@ -283,6 +283,8 @@ class MyGame(arcade.Window):
         self.torso_sprite.center_y = self.player_sprite.center_y
         self.torso_sprite.update()
         self.faceling_sprite.update()
+        for faceling_sprite in self.faceling_list:
+            faceling_sprite.follow_sprite(self.player_sprite)
         self.cursor_sprite.update()
         self.player_sprite.update(delta_time)
         self.cursor_sprite.center_x = self._mouse_x + self.get_viewport()[0]
