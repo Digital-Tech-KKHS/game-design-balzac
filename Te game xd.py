7ef6d9304ad53f03fff08212dba2a04ad2ba0b69
hyphen235 <aaaaustin12@gmail.com>
2022-03-25 14:05:30 +1300

    added test area

--- Te game xd.py
+++ Te game xd.py
@@ -3,6 +3,7 @@ from tkinter import Y
 import arcade
 import math
 from arcade.experimental.lights import Light, LightLayer
+import random
 
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 960
@@ -15,7 +16,7 @@ PLAYER_MOVEMENT_SPEED = 3
 AMBIENT_COLOR = (0, 0, 0)
 TILE_SCALING = 0.4
 SPRINT_SPEED = 2
-
+SPRITE_SPEED = 2
 
 
 def load_texture_pair(filename):
@@ -84,10 +85,17 @@ class PlayerCharacter(arcade.Sprite):
         return
         
 class faceling(arcade.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.scale = CHARACTER_SCALING
-    
+
+    def follow_sprite(self, player_sprite):
+        if self.center_y < player_sprite.center_y:
+            self.center_y += min(SPRITE_SPEED, player_sprite.center_y - self.center_y)
+        elif self.center_y > player_sprite.center_y:
+            self.center_y -= min(SPRITE_SPEED, self.center_y - player_sprite.center_y)
+
+        if self.center_x < player_sprite.center_x:
+            self.center_x += min(SPRITE_SPEED,player_sprite.center_x - self.center_x)
+        elif self.center_x > player_sprite.center_x:
+            self.center_x -= min(SPRITE_SPEED, self.center_x - player_sprite.center_x)
     
 
 
@@ -135,8 +143,8 @@ class MyGame(arcade.Window):
         self.scene['torso_list'].append(self.torso_sprite)
         faceling = "faceling.png"
         self.faceling_sprite = arcade.Sprite(faceling, CHARACTER_SCALING)
-        self.faceling_sprite.center_x = 9472
-        self.faceling_sprite.center_y = 6016
+        self.faceling_sprite.center_x = random.randrange(SCREEN_WIDTH)
+        self.faceling_sprite.center_y = random.randrange(SCREEN_HEIGHT)
         self.faceling_list.append(self.faceling_sprite)
         self.scene['faceling_list'].append(self.faceling_sprite)
         self.torso_sprite.angle = 180
@@ -258,6 +266,8 @@ class MyGame(arcade.Window):
         self.torso_sprite.center_x = self.player_sprite.center_x
         self.torso_sprite.center_y = self.player_sprite.center_y
         self.torso_sprite.update()
+        for faceling_sprite in self.faceling_list:
+            faceling.follow_sprite(self, self.player_sprite)
         self.faceling_sprite.update()
         self.cursor_sprite.update()
         self.player_sprite.update(delta_time)
