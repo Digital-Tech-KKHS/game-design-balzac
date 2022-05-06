f0876452c9a5a622ee974ae600a267e3552d1e18
hyphen235 <aaaaustin12@gmail.com>
2022-05-06 14:57:28 +1200

    added props and prop tileset

---
+++ Level 4 assets/props.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="props" tilewidth="320" tileheight="320" tilecount="100" columns="10">
+ <image source="../assets/props.png" width="3200" height="3200"/>
+</tileset>
