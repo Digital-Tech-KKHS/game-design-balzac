5c142c665f511d07da9e3dd3817f9d914f327999
Lenkwolf <puggychien@gmail.com>
2022-05-11 12:10:52 +1200

    polishing lvl2 still

---
+++ Level 2 assets/arrows.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="arrows" tilewidth="320" tileheight="320" tilecount="12" columns="6">
+ <image source="road.png" width="1920" height="640"/>
+</tileset>
