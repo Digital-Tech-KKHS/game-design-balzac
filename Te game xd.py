e025016c70b8c47246c61b80c7c8628af77241f2
Lenkwolf <puggychien@gmail.com>
2022-06-21 09:53:10 +1200

    fixed stuf

--- Te game xd.py
+++ Te game xd.py
@@ -122,7 +122,7 @@ class MyGame(arcade.View):
         self.sprintbarback = None
         self.sprintbarfore = None
         enemy_physics_engine = 0
-        self.level = 1
+        self.level = 3
 
         self.subtitle = None
         self.facesound = arcade.load_sound("assets\sounds\gacelingsound.mp3")
@@ -358,9 +358,9 @@ class MyGame(arcade.View):
  
 
         for enemy in self.scene['enemy_list']:
-            if arcade.has_line_of_sight(self.player_sprite.position , enemy.position , self.scene["walls"], max_distance=350):
+            if arcade.has_line_of_sight(self.player_sprite.position , enemy.position , self.scene["walls"], 350):
                 enemy.follow_sprite(self.player_sprite)
-                #arcade.play_sound(self.facesound, volume=0.2)
+                arcade.play_sound(self.facesound, volume=0.2)
                 start_x = enemy.center_x
                 start_y = enemy.center_y
                 dest_x = self.torso_sprite.center_x
